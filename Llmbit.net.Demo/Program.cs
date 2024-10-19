e54f8fc4c3c230ba981984c0ab4c74dcd0039e69
Sandro Hanea <sandrohanea@microsoft.com>
2024-10-19 18:53:48 +0200

    Added readme and demo

---
+++ Llmbit.net.Demo/Program.cs
@@ -0,0 +1,53 @@
+﻿using LLama.Common;
+using LLama;
+using LLama.Sampling;
+using System.Runtime.InteropServices;
+
+string modelPath = @"C:\Projects\Microsoft\BitNet\models\Llama3-8B-1.58-100B-tokens\ggml-model-i2_s.gguf"; // change it to your own model path.
+
+var llamaHandle = NativeLibrary.Load("runtimes/cpu/win-x64/llama.dll");
+
+var parameters = new ModelParams(modelPath)
+{
+    ContextSize = 1024, // The longest length of chat as memory.
+    GpuLayerCount = 5 // How many layers to offload to GPU. Please adjust it according to your GPU memory.
+};
+using var model = LLamaWeights.LoadFromFile(parameters);
+using var context = model.CreateContext(parameters);
+var executor = new InteractiveExecutor(context);
+
+// Add chat histories as prompt to tell AI how to act.
+var chatHistory = new ChatHistory();
+chatHistory.AddMessage(AuthorRole.System, "Transcript of a dialog, where the User interacts with an Assistant named Bob. Bob is helpful, kind, honest, good at writing, and never fails to answer the User's requests immediately and with precision.");
+chatHistory.AddMessage(AuthorRole.User, "Hello, Bob.");
+chatHistory.AddMessage(AuthorRole.Assistant, "Hello. How may I help you today?");
+
+ChatSession session = new(executor, chatHistory);
+
+InferenceParams inferenceParams = new InferenceParams()
+{
+    MaxTokens = 256, // No more than 256 tokens should appear in answer. Remove it if antiprompt is enough for control.
+    AntiPrompts = new List<string> { "User:" }, // Stop generation once antiprompts appear.
+
+    SamplingPipeline = new DefaultSamplingPipeline(),
+};
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Write("The chat session has started.\nUser: ");
+Console.ForegroundColor = ConsoleColor.Green;
+string userInput = Console.ReadLine() ?? "";
+
+while (userInput != "exit")
+{
+    await foreach ( // Generate the response streamingly.
+        var text
+        in session.ChatAsync(
+            new ChatHistory.Message(AuthorRole.User, userInput),
+            inferenceParams))
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write(text);
+    }
+    Console.ForegroundColor = ConsoleColor.Green;
+    userInput = Console.ReadLine() ?? "";
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Llmbit.net
+
+Open-Source llmbit.net
+
+Dotnet bindings for bitnet made possible by [bitnet.cpp](https://github.com/microsoft/BitNet/) and [llama.cpp](https://github.com/ggerganov/llama.cpp)
+
+Project abandoned as it seems it is simply possible to use [LlamaSharp](https://github.com/SciSharp/LLamaSharp) and Bitnet together. See demo in this repo. 
+
+The native dlls were built with the instructions  [bitnet.cpp](https://github.com/microsoft/BitNet/) and copied to /runtimes/Llmbit.net.Runtime.Cpu/win-x64/
+
+The 1bit models were quanitzed with the instructions in  [bitnet.cpp](https://github.com/microsoft/BitNet/) 
